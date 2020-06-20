e1e9660e06f233e99d39a71f3268cc82180d2f19
Carlsbeat <infinity183@gmail.com>
2020-06-19 18:02:41 -0700

    More changes.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,38 +1,78 @@
+# We have to start by importing the dependencies.
 import os
 import csv
 
+# Now we must read the budget_data csv file, which is in PyBank's Resources folder.
 budget_data = os.path.join("PyBank", "Resources", "budget_data.csv")
 
 with open(budget_data) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
+    # Since there's a header in this spreadsheet, we must define it.
     csv_header = next(csv_reader)
 
+    # These are the variables we will be needing for our output...
 
+    # The total number of months:
     month_counter = 0
+
+    # The total Profits/Losses across the whole period:
     net_change = 0
+
+    # The month with the highest gain:
     max_gain_month = ""
+
+    # The total profit for the highest-gaining month:
     max_gain_total = 0
+
+    # The month with the highest loss:
     max_loss_month = ""
+
+    # The total loss for the highest-losing month:
     max_loss_total = 0
 
+    # It's time to loop through the csv spreadsheet.
+
+    # We check each row in the csv
     for row in csv_reader:
+        # Each new row accounts for another month:
         month_counter += 1
+
+        # We need to convert profit/loss value for each month
+        # into an integer before applying mathematical comparisons
+        # to them (row[1] corresponds to the Profit/Losses for
+        # that month):
         row[1] = int(row[1])
+
+        # We will now add the profit/loss for the given month
+        # to our total:
         net_change += row[1]
+
+        # Next, we must check to see if the current month has the
+        # greatest profit thus far:
         if row[1] > max_gain_total:
+            # If the current month is higher than the current max gain,
+            # then we must redefine max_gain_month and max_gain_total to
+            # correspond to the current row:
             max_gain_month = row[0]
             max_gain_total = row[1]
+        # Likewise for checking for the greatest losses:
         elif row[1] < max_loss_total:
             max_loss_month = row[0]
             max_loss_total = row[1]
 
+
 #text_output = open("output.txt", "r+")
 
+# With our calculations complete, we will now print our results.
 print("Financial Analysis")
 print("----------------------------")
+# I'm using f-strings to print the redefined variables
+# more conveniently.
 print(f'Total Months: {month_counter}')
 print(f'Total: ${net_change}')
+# The average change is calculated by dividing the total profit/loss
+# by the total number of months. I am rounding to two decimal places
+# so that it's easier to read.
 print(f'Average Change: ${round(float(net_change / month_counter), 2)}')
 print(f'Greatest Increase in Profits: {max_gain_month} (${max_gain_total})')
 print(f'Greatest Decrease in Profits: {max_loss_month} (${max_loss_total})')
-
---
+++ PyPoll/main.py
@@ -0,0 +1,116 @@
+# Let's first import our dependencies.
+import os
+import csv
+
+# Now we'll open up the election_data csv file.
+election_data = os.path.join("PyPoll", "Resources", "election_data.csv")
+
+# I'm going to define my essential variables/empty lists for later.
+
+# The vote counter will start at 0, but steadily increase once we
+# open the csv and loop through it.
+number_of_votes = 0
+
+# The candidate list will include the names of each unique candidate
+# we discover in the csv.
+candidate_list = []
+
+# The candidate counter will contain vote counters with indexes that
+# directly correspond to the candidate list.
+candidate_counter = []
+
+# The candidate percentages list will make it easier for me to print
+# the percentages each candidate received more easily later.
+candidate_percentages = []
+
+# Now we will open and read the csv file.
+with open(election_data) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    # Don't forget the header!
+    csv_header = next(csv_reader)
+
+    for row in csv_reader:
+
+        # Each new row delineates a new vote, so we'll add 1 to the
+        # total number of votes:
+        number_of_votes += 1
+
+        # If we come across a candidate who hasn't yet appeared in the
+        # list of candidates, we will need to append the new candidate
+        # to our grand list of candidates (row[2] corresponds to the
+        # candidate that the current voter cast their ballot for):
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
+
+            # In addition to adding the new candidate to the list of
+            # candidate names, we must also add a new index to the
+            # list of vote totals. Since the current row is the given
+            # candidate's first vote so far, we will start the new
+            # counter at 1.
+            candidate_counter.append(1)
+
+        # If the current voter chose somebody already accounted for,
+        # then we must add 1 to that candidate's vote counter.
+        elif row[2] in candidate_list:
+
+            # This nested for loop searches for the chosen candidate
+            # in the candidate_list and uses the corresponding index
+            # to add one vote to the appropriate counter in the
+            # candidate_counter list.
+            for i in candidate_list:
+                if i == row[2]:
+
+                    # The index is that which corresponds to where the
+                    # chosen candidate is found in candidate_list.
+                    index = candidate_list.index(i)
+                    candidate_counter[index] += 1
+
+# It's time to print our results.
+
+print("FINAL RESULTS")
+print("* * * * * * * *")
+print("")
+print(f'Total Votes: {number_of_votes}')
+print("")
+
+# This for loop prints the total number of votes for each candidate.
+# It'll go through the candidate_list and use the current index to
+# extract the corresponding number of votes in candidate_counter.
+for j in candidate_list:
+    print(f'Votes for {j}: {candidate_counter[candidate_list.index(j)]}')
+
+    # Since we now have a full list of candidates and their vote grand
+    # totals, we may now calculate their vote percentages. I am building
+    # the candidate_percentages list in this for loop out of convenience.
+    # Each percentage will equal the candidate's number of votes divided
+    # by the total number of votes for all candidates.
+    candidate_percentages.append((candidate_counter[candidate_list.index(j)] / number_of_votes))
+print("")
+
+# With our candidate_percentages list complete, we'll now print a list
+# of vote percentages for each candidate.
+for k in candidate_percentages:
+    print(f'Percent Votes for {candidate_list[candidate_percentages.index(k)]}: {round(k * 100, 3)}%')
+
+# Finally, we need to peruse the vote totals for each candidate to
+# determine the winner. To do this properly, we need to find the max
+# value in the candidate_counter list before corresponding the value
+# to the winning candidate. For this, we can simply create a new
+# variable that determines the highest vote total in candidate_counter.
+highest_votes = max(candidate_counter)
+
+# With highest_votes defined, we'll now decide the winner from the
+# list of candidates.
+for l in candidate_counter:
+    
+    # The winning value has to equal highest_votes.
+    if l == highest_votes:
+
+        # From candidate_list, we must use the index for
+        # candidate_counter's highest value.
+        winner = candidate_list[candidate_counter.index(l)]
+print("")
+
+# We will now declare the winner.
+print(f'Looks like {winner} is headed for office!')
